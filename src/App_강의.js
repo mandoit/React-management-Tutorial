e94a24562959ade83668b14419f145ebd44aa2c3
kajukaju <mandoit@nate.com>
2021-09-29 12:35:40 +0900

    apply material ui

--- src/App_강의.js
+++ src/App_강의.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Customer from "./components/Customer";
 import Paper from '@material-ui/core/Paper';
@@ -11,8 +11,18 @@ import { withStyles } from '@material-ui/core/styles';
 //npm install @mui/styles-->도로지움 npm uninstall @mui/styles, npm uninstall @mui/material
 //npm install @material-ui/core 이거 설치함
 
-
-const styles = theme => ({
+/*
+const styles = theme =>({
+  root:{
+    width:'100%',
+    marginTop: theme.spacing(13),
+    overflowX:"auto"
+  },
+  table:{
+    minWidth:1080
+  }
+})*/ //안돼서 아래처럼 교체
+const styles = theme=>( {
   root: {
     width: "100%",
     marginTop: theme.spacing(3),
@@ -51,12 +61,39 @@ const customers = [{
 
 ]
 
+class App extends Component {
+  render() {
+    const { classes } = this.props;//아래 괄호안에서 classes.root 와 같이 사용할 변수를 여기에 정의 하느라 {}로 감쌈
+    return (
+      <Paper className={classes.root} >
+        <Table className={classes.table}>
+          <TableHead><tr>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell></tr>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {
+              //map 이용시는 꼭  key 를 고유값으로 넘겨라..
+              return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+}
 
-function App(prop) {
+
+
+function App_back(prop) {
   const { classes } = prop;
   return (
-    <Paper className={classes.root} >
-      <Table className={classes.table}>
+    <Paper className="root" >
+      <Table className="table">
         <TableHead><tr>
           <TableCell>번호</TableCell>
           <TableCell>이미지</TableCell>
